9ef7a5fcbca616f665f99fb1f576a30d7407e62f
tpauldike <topman4loveworld@gmail.com>
2023-08-25 08:10:51 +0000

    functions in C

--- 6-function/function.c
+++ 6-function/function.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+/*
+  1 + 23 = 24
+  6 + 5 = 11
+  7 + 1
+  8 + 10
+  12 + 19
+  10 + 12
+ */
 int main(void)
 {
+	int num1 = 1, num2 = 23;
 
-	printf();
+	int sumOfNumbers = num1 + num2;
+	printf("%d + %d = %d\n", num1, num2, sumOfNumbers);
 
 	return (0);
 }
