e3a998d0f8607cd472494ba0330be0d8c4b12fa5
Olumide Solanke <olumide@greyfusion.com.ng>
2023-09-01 08:41:10 +0100

    _putchar

---
+++ 6-function/_write_putchar.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	write("_putchar\n");
+
+    return (0);
+}
+
---
+++ 6-function/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN H_
+#define _MAIN H_
+
+
+/* Header Functions */
+#include <stdio.h>
+
+/* Function Prototypes */
+int _putchar(char c);
+
+#endif /* MAIN_H */ 
