9c2d10745b5cd80b12c5bb6c31ef2375b16388d2
tpauldike <topman4loveworld@gmail.com>
2023-09-15 08:16:46 +0000

    strtok

---
+++ 8-simple_shell/strtok.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char bash_cmd[] = "ls -l";
+	char *delim = " ";
+
+	strtok
+	
+	/*
+	char str[] = "We are learning together";
+	char *segment;
+
+	segment = strtok(str, " ");
+	printf("%s\n", segment);
+
+	segment = strtok(NULL, " ");
+	printf("%s\n", segment);
+
+	segment = strtok(NULL, " ");
+	printf("%s\n", segment);
+
+	segment = strtok(NULL, " ");
+	printf("%s\n", segment);
+	*/
+
+	return (0);
+}
